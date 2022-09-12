ac313e06b556ab2086a236ce19af0bb268002c35
Raphael Rott <78685408+rottifant@users.noreply.github.com>
2022-09-12 17:11:53 +0200

    Upload Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Media;
+
+namespace outro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer player = new SoundPlayer("outro.wav");
+            player.Play();
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("Shutting down in " + i + " seconds!");
+                Thread.Sleep(1000);
+            }
+            Process.Start("shutdown", "/s");
+        }
+    }
+}
